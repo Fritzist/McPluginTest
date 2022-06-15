0a67cf62cd06c815fd0d91881e9f4d364cb91f71
Fritzist <danielhartl856@gmail.com>
2022-06-15 22:17:28 +0200

    finished the rocket(pp) command

--- src/main/java/de/fritzist/test/commands/RocketCommand.java
+++ src/main/java/de/fritzist/test/commands/RocketCommand.java
@@ -1,15 +1,46 @@
 package de.fritzist.test.commands;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class RocketCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        if (sender instanceof Player){
+            Player player = (Player) sender;
 
+            Location loc = player.getLocation();
+            loc.setY(loc.getY() + 0);
+            loc.setZ(loc.getZ() + 2);
+            loc.setX(loc.getX() + 0);
+            loc.getBlock().setType(Material.WHITE_STAINED_GLASS);
+
+            loc.setY(loc.getY() + 0);
+            loc.setZ(loc.getZ() + 0);
+            loc.setX(loc.getX() + 1);
+            loc.getBlock().setType(Material.WHITE_STAINED_GLASS);
+
+            loc.setY(loc.getY() + 0);
+            loc.setZ(loc.getZ() + 0);
+            loc.setX(loc.getX() + 1);
+            loc.getBlock().setType(Material.WHITE_STAINED_GLASS);
+
+            loc.setY(loc.getY() + 1);
+            loc.setZ(loc.getZ() + 0);
+            loc.setX(loc.getX() + -1);
+            loc.getBlock().setType(Material.WHITE_STAINED_GLASS);
+
+            loc.setY(loc.getY() + 1);
+            loc.setZ(loc.getZ() + 0);
+            loc.setX(loc.getX() + 0);
+            loc.getBlock().setType(Material.WHITE_STAINED_GLASS);
+            }
 
         return true;
     }
